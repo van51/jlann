56df643b795d0b244662d948642588ccf82f0a52
Evangelos Anagnostopoulos <aneva@di.uoa.gr>
2017-04-12 15:10:24 +0300

    First code commit

---
+++ include/jlann_ds.hpp
@@ -0,0 +1,161 @@
+/**
+ * Copyright 2017 Evangelos Anagnostopoulos
+   Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+#ifndef __JLANN_DS_HPP__
+#define __JLANN_DS_HPP__
+
+#include <Eigen/Dense>
+#include <ANN/ANN.h>
+#include <iostream>
+#include <cmath>
+#include <random>
+
+#include <sys/resource.h>
+
+namespace jlann{
+
+typedef Eigen::Matrix<double, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> DMatrix;
+typedef Eigen::Matrix<float, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> FMatrix;
+
+template<typename T>
+class JLAnnDS {
+public:
+	typedef Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> JLMatrix;
+	typedef Eigen::Matrix<T, 1, Eigen::Dynamic, Eigen::RowMajor> JLVector;
+
+	JLAnnDS(JLMatrix& dataPoints, int numOfDs=1, int projectedDimension=0, int searchSize=0):
+		originalPoints(dataPoints),
+		numberOfDs(numOfDs),
+		projDimension(projectedDimension),
+		projSearchSize(searchSize) {
+
+		numberOfPoints = originalPoints.rows();
+		dimension = originalPoints.cols();
+
+		if (projDimension<=0) {
+			projDimension = (int)std::ceil(std::log2(numberOfPoints)/(std::log2(std::log2(numberOfPoints))));
+		}
+		if (numberOfDs<=0) {
+			numberOfDs = 1;
+		}
+		if (projSearchSize<=0) {
+			projSearchSize = (int)std::ceil(std::sqrt(numberOfPoints));
+		}
+
+		annIdx = new ANNidx[projSearchSize];
+		dists = new ANNdist[projSearchSize];
+		annQueryPt = annAllocPt(projDimension);
+
+
+		//createDs();
+	}
+
+	void createDs() {
+		this->bdTree.reserve(numberOfDs);
+		this->projectionMatrices.reserve(numberOfDs);
+        std::mt19937 generator;
+        std::normal_distribution<double> distribution(0.0,1.0);
+		struct rlimit limit;
+
+		for (int currentDs=0; currentDs<numberOfDs; currentDs++) {
+			JLMatrix* projMatrix = new JLMatrix(dimension, projDimension);
+
+        	for (int i=0; i<dimension; i++) {
+        	    for (int j=0; j<projDimension; j++) {
+        	        (*projMatrix)(i, j) = distribution(generator);
+        	    }
+        	}
+			JLMatrix projectedData = originalPoints*(*projMatrix);
+
+        	ANNpointArray point_array = annAllocPts(numberOfPoints, projDimension);
+			auto dataPointer = projectedData.data();
+       	    for (int i=0; i<numberOfPoints; i++) {
+	        	for (int j=0; j<projDimension; j++) {
+        	        point_array[i][j] = (*dataPointer);
+					++dataPointer;
+        	    }
+        	}
+
+			this->projectionMatrices.push_back(projMatrix);
+			this->pointArray.push_back(point_array);
+			ANNbd_tree* bdtree = new ANNbd_tree(
+        	    point_array,
+        	    numberOfPoints,
+        	   	projDimension,
+				2
+        	);
+			this->bdTree.push_back(bdtree);
+		}
+	}
+
+	int findNearestNeighbor(JLVector& queryPoint, double epsilon) {
+
+		int minDistance=-1;
+		int nearestNeighborIdx=-1;
+		for (int currentDs=0; currentDs<numberOfDs; currentDs++) {
+        	for (int i=0; i<projDimension; i++) {
+				annQueryPt[i] = 0;
+        	    for (int j=0; j<dimension; j++) {
+        	        annQueryPt[i] += queryPoint(j) * (*projectionMatrices[currentDs])(j, i);
+        	    }
+        	}
+        	this->bdTree[currentDs]->annkSearch(
+        	    annQueryPt,
+        	    projSearchSize,
+        	    annIdx,
+        	    dists,
+        	    epsilon
+        	);
+
+        	for (int i=0; i<projSearchSize; i++) {
+        	    if (annIdx[i]==-1) {
+        	        break;
+        	    }
+				double distance = (queryPoint-originalPoints.row(annIdx[i])).norm();
+        	    if (distance<minDistance || minDistance<0) {
+        	        minDistance = distance;
+        	        nearestNeighborIdx = annIdx[i];
+        	    }
+        	}
+		}
+
+		return nearestNeighborIdx;
+	}
+
+	~JLAnnDS() {
+		for (int i=0; i<pointArray.size(); i++) {
+			annDeallocPts(pointArray[i]);
+		}
+		for (int i=0; i<projectionMatrices.size(); i++) {
+			delete projectionMatrices[i];
+		}
+		for (int i=0; i<bdTree.size(); i++) {
+			delete bdTree[i];
+		}
+		annDeallocPt(annQueryPt);
+		delete []annIdx;
+		delete []dists;
+	}
+
+private:
+	std::vector<JLMatrix*> projectionMatrices;
+	std::vector<ANNbd_tree*> bdTree;
+	JLMatrix& originalPoints;
+	std::vector<ANNpointArray> pointArray;
+	ANNidxArray annIdx;
+	ANNdistArray dists;
+	ANNpoint annQueryPt;
+	int numberOfDs;
+	int projDimension;
+	int projSearchSize;
+	int dimension;
+	int numberOfPoints;
+}; //class JLAnnDs
+} //namespace jlann
+#endif //__JLANN_DS_HPP__
---
+++ src/main.cpp
@@ -0,0 +1,173 @@
+#include "jlann_ds.hpp"
+#include "ANN/ANN.h"
+#include <iostream>
+#include <cmath>
+#include <fstream>
+#include <Eigen/Dense>
+#include <string>
+#include <boost/program_options.hpp>
+#include <sstream>
+#include <chrono>
+#include <sys/resource.h>
+
+using namespace std;
+namespace po = boost::program_options;
+
+using dtype = double;
+using JLMatrix = typename jlann::JLAnnDS<dtype>::JLMatrix;
+using JLVector = typename jlann::JLAnnDS<dtype>::JLVector;
+
+class Timer {
+public:
+	Timer() { start_time = std::chrono::high_resolution_clock::now(); }
+
+	void start() {
+		start_time = std::chrono::high_resolution_clock::now();
+	}
+
+	double end() {
+		return elapsed_seconds();
+	}
+
+	double elapsed_seconds() {
+		auto end_time = std::chrono::high_resolution_clock::now();
+		auto elapsed = std::chrono::duration_cast<std::chrono::duration<double>>(end_time - start_time);
+		return elapsed.count();
+	}
+private:
+	decltype(std::chrono::high_resolution_clock::now()) start_time;
+};
+
+template <typename T>
+void parse_csv(string filename, Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>& dataset) {
+	ifstream fin(filename.c_str());
+	int numberOfPoints;
+	int dimension;
+	fin >> numberOfPoints >> dimension;
+
+	dataset.resize(numberOfPoints, dimension);
+
+	string line;
+	string line2;
+	getline(fin, line);
+	int i=0;
+	while (getline(fin, line)) {
+		istringstream iss(line);
+		int j=0;
+		while (getline(iss, line2, ',')) {
+			T tmp;
+			istringstream(line2) >> tmp;
+			dataset(i, j++) = tmp;
+			//point_array[i][j++] = tmp;
+		}
+		i++;
+		if (i==numberOfPoints) {
+			break;
+		}
+	}
+	fin.close();
+}
+
+template <typename T>
+void read_file(string filename, Eigen::Matrix<T, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>& dataset) {
+	if (filename.find(".csv")!=string::npos) {
+		parse_csv<T>(filename, dataset);
+	}
+}
+
+void queryJl(jlann::JLAnnDS<dtype>* jlAnnDs, double epsilon, JLMatrix& dataset, JLMatrix& queries,
+				Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor>& groundtruth) {
+	int numberOfQueries = queries.rows();
+	int trueNn = 0;
+	int ann = 0;
+	double annQueryTime = 0;
+	for (int i=0; i<numberOfQueries; i++) {
+		Timer timer;
+		JLVector query = queries.row(i);
+		int nnIndex = jlAnnDs->findNearestNeighbor(query, epsilon);
+		annQueryTime += timer.elapsed_seconds();
+
+		double nnDist = (dataset.row(groundtruth(i, 0))-query).norm();
+		double annDist = (dataset.row(nnIndex)-query).norm();
+		//cout << annDist << " <= " << "(1+" << epsilon << ")*" << nnDist << " = " << (1+epsilon)*nnDist << endl;
+
+		if (nnIndex==groundtruth(i, 0)) {
+			++trueNn;
+		}
+		for (int j=1; j<groundtruth.cols(); j++) {
+			if (nnIndex==groundtruth(i, j)) {
+				++ann;
+			}
+		}
+	}
+
+	cout << "Total query time: " << annQueryTime << " -- Average query time: " << annQueryTime/numberOfQueries << endl;
+	cout << "True nns percentage: " << (double)trueNn/numberOfQueries*100 << "%" << endl;
+	cout << "Misses percentage: " << (double)(numberOfQueries-ann)/numberOfQueries*100 << "%" << endl;
+}
+
+int main(int argc, char* argv[]) {
+	boost::program_options::options_description desc;
+	desc.add_options()
+		("input,i", po::value<string>(), "The input file (can be .ine, .csv); mandatory.")
+		("query,q", po::value<string>(), "The query file (can be .ine, .csv); mandatory.")
+		("groundtruth,g", po::value<string>(), "The groundtruth file (can be .ine, .csv); mandatory.")
+		("e", po::value<double>()->value_name("Val")->default_value(0.1), "The approximation factor")
+	;
+
+	po::variables_map vm;
+	po::store(po::parse_command_line(argc, argv, desc), vm);
+	po::notify(vm);
+
+	if (vm.count("input")==0) {
+		cout << desc << endl;
+		return 1;
+	}
+
+	if (vm.count("query")==0) {
+		cout << desc << endl;
+		return 1;
+	}
+
+	if (vm.count("groundtruth")==0) {
+		cout << desc << endl;
+		return 1;
+	}
+
+	if (vm.count("e")==0) {
+		cout << desc << endl;
+		return 1;
+	}
+
+	string filename = vm["input"].as<string>();
+	string query_filename = vm["query"].as<string>();
+	string groundtruth_filename = vm["groundtruth"].as<string>();
+	double epsilon = vm["e"].as<double>();
+
+	cout << "Reading files. " << endl;
+	JLMatrix dataset;
+	read_file<dtype>(filename, dataset);
+	cout << "Read dataset." << endl;
+
+	JLMatrix queries;
+	read_file<dtype>(query_filename, queries);
+	cout << "Read queries." << endl;
+
+	Eigen::Matrix<int, Eigen::Dynamic, Eigen::Dynamic, Eigen::RowMajor> groundtruth;
+	read_file<int>(groundtruth_filename, groundtruth);
+	cout << "Read groundtruth." << endl;
+
+	Timer timer;
+	std::cout << "Creating jlann ds." << std::endl;
+	int projDimension = 16;//(int)2*std::ceil(std::log2(dataset.rows())/(std::log2(std::log2(dataset.rows()))));
+	int projSearchSize = (int)std::sqrt(dataset.rows());
+	jlann::JLAnnDS<dtype>* jlAnnDs = new jlann::JLAnnDS<dtype>(dataset, 1, projDimension, projSearchSize);
+	jlAnnDs->createDs();
+	double jlannPreprocessingTime = timer.end();
+	cout << "Constructing jlann dataset took " << jlannPreprocessingTime << "s" << endl;
+
+	queryJl(jlAnnDs, epsilon, dataset, queries, groundtruth);
+
+    delete jlAnnDs;
+	annClose();
+}
